6e48ccb6e41003c35db3f97115891bac7ffcecfb
Samariran <sunluyong@gmail.com>
2015-07-12 21:40:57 +0800

    less

--- projects/express/app.js
+++ projects/express/app.js
@@ -6,10 +6,12 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	res.send('Hello World!');
+	var callback = req.query.callback || 'callback';
+	res.set('Content-Type', 'text/javascript');
+	res.send(callback + "([{name: 'Book 1'},{name: 'Book 2'},{name: 'Book 3'},{name: 'Book 4'},{name: 'Book 5'}]);");
 });
 
-var server = app.listen(3000, '127.0.0.1', function(){
+var server = app.listen(3001, '127.0.0.1', function(){
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Example app listening at http://%s:%s', host, port);
