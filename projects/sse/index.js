a92dfb4177243cd901a85daebfe1f2807ece9fc2
Samariran <sunluyong@gmail.com>
2015-07-30 21:46:48 +0800

    sse

--- projects/sse/index.js
+++ projects/sse/index.js
@@ -12,7 +12,12 @@ http.createServer(function (req, res) {
     fileName = "." + req.url;
 
   if (fileName === "./stream") {
-    res.writeHead(200, {"Content-Type":"text/event-stream", "Cache-Control":"no-cache", "Connection":"keep-alive"});
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      "Connection":"keep-alive"
+    });
+
     res.write("retry: 10000\n");
     res.write("event: connecttime\n");
     res.write("data: " + (new Date()) + "\n\n");
@@ -21,9 +26,11 @@ http.createServer(function (req, res) {
     interval = setInterval(function() {
       res.write("data: " + (new Date()) + "\n\n");
     }, 1000);
+
     req.connection.addListener("close", function () {
       clearInterval(interval);
     }, false);
+
   } else if (fileName === index) {
     fs.exists(fileName, function(exists) {
       if (exists) {
